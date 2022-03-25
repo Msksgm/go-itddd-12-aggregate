e4301a564de6160df194c415d50624b2127ee99c
Msksgm <msksgm3124@gmail.com>
2022-03-26 08:44:33 +0900

    add: 3文字以上のバリデーションを追加

--- domain/model/user/username.go
+++ domain/model/user/username.go
@@ -1,9 +1,14 @@
 package user
 
+import "fmt"
+
 type UserName struct {
 	value string
 }
 
 func NewUserName(value string) (*UserName, error) {
+	if len(value) < 3 {
+		return nil, fmt.Errorf("UserName is more than 3 characters.")
+	}
 	return &UserName{value: value}, nil
 }
--- domain/model/user/username_test.go
+++ domain/model/user/username_test.go
@@ -7,13 +7,22 @@ import (
 )
 
 func Test_NewUserName(t *testing.T) {
-	got, err := NewUserName("username")
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("success", func(t *testing.T) {
+		got, err := NewUserName("username")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	want := &UserName{value: "username"}
-	if diff := cmp.Diff(want, got, cmp.AllowUnexported(UserName{})); diff != "" {
-		t.Errorf("mismatch (-want, +got):\n%s", diff)
-	}
+		want := &UserName{value: "username"}
+		if diff := cmp.Diff(want, got, cmp.AllowUnexported(UserName{})); diff != "" {
+			t.Errorf("mismatch (-want, +got):\n%s", diff)
+		}
+	})
+	t.Run("fail", func(t *testing.T) {
+		_, err := NewUserName("us")
+		want := "UserName is more than 3 characters."
+		if got := err.Error(); got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
