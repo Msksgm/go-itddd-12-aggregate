c1dc98539954008c52de3f6f0bef3588f3d50f64
Msksgm <msksgm3124@gmail.com>
2022-03-27 06:16:22 +0900

    add: IsFull() < 29 のときのテストをついか

--- domain/model/circle/circle_test.go
+++ domain/model/circle/circle_test.go
@@ -1,7 +1,6 @@
 package circle
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Msksgm/go-itddd-12-aggregate/domain/model/user"
@@ -50,51 +49,102 @@ func Test_NewCircle(t *testing.T) {
 }
 
 func Test_IsFull(t *testing.T) {
-	ownerId, err := user.NewUserId("ownerId")
-	if err != nil {
-		t.Fatal(err)
-	}
-	ownerName, err := user.NewUserName("ownerName")
-	if err != nil {
-		t.Fatal(err)
-	}
-	owner, err := user.NewUser(*ownerId, *ownerName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	memberId, err := user.NewUserId("memberId")
-	if err != nil {
-		t.Fatal(err)
-	}
-	memberName, err := user.NewUserName("memberName")
-	if err != nil {
-		t.Fatal(err)
-	}
-	member, err := user.NewUser(*memberId, *memberName)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("full", func(t *testing.T) {
+		ownerId, err := user.NewUserId("ownerId")
+		if err != nil {
+			t.Fatal(err)
+		}
+		ownerName, err := user.NewUserName("ownerName")
+		if err != nil {
+			t.Fatal(err)
+		}
+		owner, err := user.NewUser(*ownerId, *ownerName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		memberId, err := user.NewUserId("memberId")
+		if err != nil {
+			t.Fatal(err)
+		}
+		memberName, err := user.NewUserName("memberName")
+		if err != nil {
+			t.Fatal(err)
+		}
+		member, err := user.NewUser(*memberId, *memberName)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	circleId, err := NewCircleId("circleId")
-	if err != nil {
-		t.Fatal(err)
-	}
-	circleName, err := NewCircleName("circlename")
-	if err != nil {
-		t.Fatal(err)
-	}
+		circleId, err := NewCircleId("circleId")
+		if err != nil {
+			t.Fatal(err)
+		}
+		circleName, err := NewCircleName("circlename")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	members := []user.User{*owner}
+		members := []user.User{*owner}
 
-	for i := 0; i < 30; i++ {
-		members = append(members, *member)
-	}
+		for i := 0; i < 30; i++ {
+			members = append(members, *member)
+		}
 
-	circle, err := NewCircle(*circleId, *circleName, *owner, members)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if circle.IsFull() {
-		fmt.Println("circle must be less than 29 members")
-	}
+		circle, err := NewCircle(*circleId, *circleName, *owner, members)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !circle.IsFull() {
+			t.Error("circle must be less than 29 members")
+		}
+	})
+	t.Run("not full", func(t *testing.T) {
+		ownerId, err := user.NewUserId("ownerId")
+		if err != nil {
+			t.Fatal(err)
+		}
+		ownerName, err := user.NewUserName("ownerName")
+		if err != nil {
+			t.Fatal(err)
+		}
+		owner, err := user.NewUser(*ownerId, *ownerName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		memberId, err := user.NewUserId("memberId")
+		if err != nil {
+			t.Fatal(err)
+		}
+		memberName, err := user.NewUserName("memberName")
+		if err != nil {
+			t.Fatal(err)
+		}
+		member, err := user.NewUser(*memberId, *memberName)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		circleId, err := NewCircleId("circleId")
+		if err != nil {
+			t.Fatal(err)
+		}
+		circleName, err := NewCircleName("circlename")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		members := []user.User{*owner}
+
+		for i := 0; i < 20; i++ {
+			members = append(members, *member)
+		}
+
+		circle, err := NewCircle(*circleId, *circleName, *owner, members)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if circle.IsFull() {
+			t.Error("circle must be less than 29 members")
+		}
+	})
 }
