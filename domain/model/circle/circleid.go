9fe99e3259b4d6bb02ee8397f3813a3caaf28250
Msksgm <msksgm3124@gmail.com>
2022-03-26 10:09:18 +0900

    add: circleId.Equals と circleId.String を追加

--- domain/model/circle/circleid.go
+++ domain/model/circle/circleid.go
@@ -1,9 +1,22 @@
 package circle
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type CircleId struct {
 	value string
 }
 
 func NewCircleId(id string) (*CircleId, error) {
 	return &CircleId{value: id}, nil
+}
+
+func (circleId *CircleId) Equals(other *CircleId) bool {
+	return reflect.DeepEqual(circleId.value, other.value)
+}
+
+func (circleId *CircleId) String() string {
+	return fmt.Sprintf("CircleId [value: %s]", circleId.value)
 }
--- domain/model/circle/circleid_test.go
+++ domain/model/circle/circleid_test.go
@@ -1,6 +1,7 @@
 package circle
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -15,5 +16,44 @@ func Test_NewCircleId(t *testing.T) {
 
 	if diff := cmp.Diff(want, got, cmp.AllowUnexported(CircleId{})); diff != "" {
 		t.Errorf("mismatch (-want, +got):\n%s", diff)
+	}
+}
+
+func Test_CircleIdEquals(t *testing.T) {
+	circleId, err := NewCircleId("id")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Run("equal", func(t *testing.T) {
+		otherCircleId, err := NewCircleId("id")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !circleId.Equals(otherCircleId) {
+			t.Errorf("circleId: %v must be equal to otherCircleId: %v", circleId, otherCircleId)
+		}
+	})
+	t.Run("not equal", func(t *testing.T) {
+		otherCircleId, err := NewCircleId("otherId")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if circleId.Equals(otherCircleId) {
+			t.Errorf("circleId: %v must be equal to otherCircleId: %v", circleId, otherCircleId)
+		}
+	})
+}
+
+func Test_CircleIdString(t *testing.T) {
+	circleId, err := NewCircleId("id")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := fmt.Sprint(circleId)
+	want := fmt.Sprintf("CircleId [value: %s]", circleId.value)
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
 	}
 }
