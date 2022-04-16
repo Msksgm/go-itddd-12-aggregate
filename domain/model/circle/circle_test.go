543207a96e7d693c8d63105c687be475020454c8
Msksgm <msksgm3124@gmail.com>
2022-04-16 15:37:27 +0900

    fix: 重複を削除

--- domain/model/circle/circle_test.go
+++ domain/model/circle/circle_test.go
@@ -51,41 +51,40 @@ func Test_NewCircle(t *testing.T) {
 }
 
 func Test_IsFull(t *testing.T) {
+	ownerId, err := user.NewUserId("ownerId")
+	if err != nil {
+		t.Fatal(err)
+	}
+	ownerName, err := user.NewUserName("ownerName")
+	if err != nil {
+		t.Fatal(err)
+	}
+	owner, err := user.NewUser(*ownerId, *ownerName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	memberId, err := user.NewUserId("memberId")
+	if err != nil {
+		t.Fatal(err)
+	}
+	memberName, err := user.NewUserName("memberName")
+	if err != nil {
+		t.Fatal(err)
+	}
+	member, err := user.NewUser(*memberId, *memberName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	circleId, err := NewCircleId("circleId")
+	if err != nil {
+		t.Fatal(err)
+	}
+	circleName, err := NewCircleName("circlename")
+	if err != nil {
+		t.Fatal(err)
+	}
 	t.Run("full", func(t *testing.T) {
-		ownerId, err := user.NewUserId("ownerId")
-		if err != nil {
-			t.Fatal(err)
-		}
-		ownerName, err := user.NewUserName("ownerName")
-		if err != nil {
-			t.Fatal(err)
-		}
-		owner, err := user.NewUser(*ownerId, *ownerName)
-		if err != nil {
-			t.Fatal(err)
-		}
-		memberId, err := user.NewUserId("memberId")
-		if err != nil {
-			t.Fatal(err)
-		}
-		memberName, err := user.NewUserName("memberName")
-		if err != nil {
-			t.Fatal(err)
-		}
-		member, err := user.NewUser(*memberId, *memberName)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		circleId, err := NewCircleId("circleId")
-		if err != nil {
-			t.Fatal(err)
-		}
-		circleName, err := NewCircleName("circlename")
-		if err != nil {
-			t.Fatal(err)
-		}
-
 		members := []user.User{*owner}
 
 		for i := 0; i < 30; i++ {
@@ -101,40 +100,6 @@ func Test_IsFull(t *testing.T) {
 		}
 	})
 	t.Run("not full", func(t *testing.T) {
-		ownerId, err := user.NewUserId("ownerId")
-		if err != nil {
-			t.Fatal(err)
-		}
-		ownerName, err := user.NewUserName("ownerName")
-		if err != nil {
-			t.Fatal(err)
-		}
-		owner, err := user.NewUser(*ownerId, *ownerName)
-		if err != nil {
-			t.Fatal(err)
-		}
-		memberId, err := user.NewUserId("memberId")
-		if err != nil {
-			t.Fatal(err)
-		}
-		memberName, err := user.NewUserName("memberName")
-		if err != nil {
-			t.Fatal(err)
-		}
-		member, err := user.NewUser(*memberId, *memberName)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		circleId, err := NewCircleId("circleId")
-		if err != nil {
-			t.Fatal(err)
-		}
-		circleName, err := NewCircleName("circlename")
-		if err != nil {
-			t.Fatal(err)
-		}
-
 		members := []user.User{*owner}
 
 		for i := 0; i < 20; i++ {
