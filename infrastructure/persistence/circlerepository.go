9a78768edb9f1cbf40697867e28efc2b3731b141
Msksgm <msksgm3124@gmail.com>
2022-04-16 16:01:45 +0900

    fix

--- infrastructure/persistence/circlerepository.go
+++ infrastructure/persistence/circlerepository.go
@@ -15,6 +15,41 @@ func NewCircleRepository(db *sql.DB) (*CircleRepository, error) {
 	return &CircleRepository{db: db}, nil
 }
 
-func (cr *CircleRepository) FindByCircleName(ctx context.Context, circleName *circle.CircleName) (*circle.Circle, error) {
-	return &circle.Circle{}, nil
+type FindByCircleNameQueryError struct {
+	CircleName string
+	Message    string
+	Err        error
+}
+
+func (err *FindByCircleNameQueryError) Error() string {
+	return err.Message
+}
+
+func (cr *CircleRepository) FindByCircleName(ctx context.Context, circleName *circle.CircleName) (circle *circle.Circle, err error) {
+	tx, err := cr.db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		switch err {
+		case nil:
+			err = tx.Commit()
+		default:
+			tx.Rollback()
+		}
+	}()
+
+	rows, err := tx.Query("SELECT * FROM circles WHERE circlename = $1", circleName.Value())
+	if err != nil {
+		return nil, &FindByCircleNameQueryError{CircleName: circleName.Value(), Message: "error is occured in circlerepository.FindByCircleName", Err: err}
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return circle, nil
 }
