07aa2d472f0b6cd8e8baeb9b9ff87b065d34f151
Msksgm <msksgm3124@gmail.com>
2022-04-27 07:53:55 +0900

    add: 初期値を追加

--- migrations/000001_user.up.sql
+++ migrations/000001_user.up.sql
@@ -5,4 +5,7 @@ CREATE TABLE IF NOT EXISTS users(
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
+INSERT INTO users(id, name) VALUES ('ownerId', 'ownerName');
+INSERT INTO users(id, name) VALUES ('userId', 'userName');
+
 COMMIT;
